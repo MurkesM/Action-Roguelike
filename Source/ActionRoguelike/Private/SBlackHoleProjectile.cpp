b90ce933c622c35d3802bcc7677b3e764ed631bf
MurkesM <91104156+MurkesM@users.noreply.github.com>
2024-08-23 16:33:54 -0500

    fixed build warning

--- Source/ActionRoguelike/Private/SBlackHoleProjectile.cpp
+++ Source/ActionRoguelike/Private/SBlackHoleProjectile.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PhysicsEngine/RadialForceComponent.h"
 #include "SBlackHoleProjectile.h"
+#include "PhysicsEngine/RadialForceComponent.h"
 
 // Sets default values
 ASBlackHoleProjectile::ASBlackHoleProjectile()
